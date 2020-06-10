89f05379d12e1550f8e68a00bf221b3d854f92cb
Denys Meloshyn <denys.meloshyn@gmail.com>
2020-06-10 23:14:28 +0200

    Remove unused comments

--- step.sh
+++ step.sh
@@ -4,22 +4,10 @@ set -ex
 THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 python "${THIS_SCRIPT_DIR}/jira_number_presenter.py" ${BITRISE_GIT_BRANCH} ${bitrise_jira_number_prefix} ${bitrise_jira_number_should_fail}
 
-#echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
 #
 # --- Exit codes:
 # The exit code of your Step is very important. If you return
 #  with a 0 exit code `bitrise` will register your Step as "successful".
 # Any non zero exit code will be registered as "failed" by `bitrise`.
+
+exit 0
