f49e786137a5a2375cd9aba683b2e4b7bc99742a
Denys Meloshyn <denys.meloshyn@gmail.com>
2020-06-10 23:27:01 +0200

    Correct format of output value

--- jira_number_presenter.py
+++ jira_number_presenter.py
@@ -6,10 +6,12 @@ import jira_number
 
 git_branch = sys.argv[1]
 jira_prefix = sys.argv[2]
-is_exit_number_missed = bool(sys.argv[3])
+is_exit_number_missed = sys.argv[3] == 'true'
 
 jira_number = jira_number.extract_jira_number(git_branch=git_branch, jira_prefix=jira_prefix)
 if jira_number is None and is_exit_number_missed:
     sys.exit(1)
+elif jira_number is None:
+    jira_number = ''
 
-os.system('envman add --key JIRA_ISSUE_NUMBER_FROM_BRANCH --value {}'.format(jira_number))
+os.system('envman add --key JIRA_ISSUE_NUMBER_FROM_BRANCH --value "{}"'.format(jira_number))
