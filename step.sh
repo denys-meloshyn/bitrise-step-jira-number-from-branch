4f53a5670acaa966fdd066e59d441cf5be26950d
Denys Meloshyn <denys.meloshyn@gmail.com>
2020-06-11 20:00:48 +0200

    Extract branch name as input property

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 set -ex
 
 THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-python "${THIS_SCRIPT_DIR}/jira_number_presenter.py" ${BITRISE_GIT_BRANCH} ${bitrise_jira_number_prefix} ${bitrise_jira_number_should_fail}
+python "${THIS_SCRIPT_DIR}/jira_number_presenter.py" ${bitrise_jira_branch_name} ${bitrise_jira_number_prefix} ${bitrise_jira_number_should_fail}
 
 #
 # --- Exit codes:
